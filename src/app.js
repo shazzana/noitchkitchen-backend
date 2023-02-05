1284f3fa9ba1123f78d517597059b9b58987d64d
Shazzana <shazzana.shahar@gmail.com>
2023-02-05 11:02:51 +0800

    backend structurecreated

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const index = require('./routers/index');
+// const ingredientRoute = require('./routers/ingredient-router');
+
+
+// app.use(express.urlencoded({ extended: true })); ===> Is this for typescript?
+app.use(express.json());
+// app.use(express.json({ type: 'application/vnd.api+json' })); Is this typescript too?
+app.use(cors());
+
+app.use(index);
+// app.use('/api/', ingredientRoute);
+
+module.exports = app;
