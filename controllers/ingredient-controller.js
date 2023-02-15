b92b9de7af3cc8459464db341aff8008334d3db9
Shazzana <shazzana.shahar@gmail.com>
2023-02-16 00:04:18 +0800

    ringredient routes work

--- controllers/ingredient-controller.js
+++ controllers/ingredient-controller.js
@@ -1,31 +1,16 @@
 const { Ingredient } = require("../models");
 const httpStatus = require("http-status");
-const db = ("../db/conn")
+// const { response } = require("express");
+const db = ("../db/conn");
 
-const create = async (req, response) => {
-    try {
-        let db_connect = db.getDb("ingredients");
-        let myObj = {
-            id: req.body.id,
-            name: req.body.name,
-            remarks: req.body.remarks,
-            histamine: req.body.histamine,
-            otherAmines: req.body.other_amines,
-            liberator: req.body.liberator,
-            blocker: req.body.blocker,
-            compatibility: req.body.compatibility,
-            yourCompatibility: req.body.your_compatibility,
-            category: req.body.category,
-        };
-
-        db_connect.collection("ingredients").insertOne(myObj, function (err, res) {
-            if (err) throw err;
-            response.json(res);
-        });
-    } catch (e) {
-        console.error(e);
-        response.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-    }
+const create = async (req, res) => {
+  try {
+    const result = await Ingredient.create(req.body)
+    res.json(result);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
 };
 
 const findAll = async (req, res) => {
@@ -40,44 +25,25 @@ const findAll = async (req, res) => {
   };
 
 
-const updateOne = async (req, response) => {
-    try {
-      console.log(req.body);
-      let db_connect = db.getDb("ingredients");
-      let ingredientId = req.params.id;
-      let newIngredient = { id: ingredientId ,
-       $set: req.body }
+const updateOne = async (req, res) => {
+  try {
+    const ingredientId = req.params.id;
+    const updated = await Ingredient.updateOne(
+      { id: ingredientId },
+      { $set: req.body }
+    );
+    res.json(updated);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+};
 
-      ;
-  
-      db_connect
-        .collection("ingredients")
-        .updateOne({ id: ingredientId }, newIngredient, function (err, res) {
-          if (err) throw err;
-          console.log("1 document updated");
-          response.json(res);
-        });
-    } catch (e) {
-      console.error(e);
-      response.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
-    }
-  };
-
-  const deleteOne = async (req, response) => {
+  const deleteOne = async (req, res) => {
     try {
-      let db_connect = db.getDb("ingredients");
-      console.log(req.params);
       const ingredientId = req.params.id;
-      console.log(ingredientId);
-
-      db_connect.collection("ingredients").deleteOne(
-        { id: ingredientId },
-        function (err, res) {
-          if (err) throw err;
-          console.log("1 document updated");
-          response.json(res);
-        }
-      );
+      const result = await Ingredient.deleteOne({ id: ingredientId })
+      res.json(result);
     } catch (e) {
       console.error(e);
       res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
